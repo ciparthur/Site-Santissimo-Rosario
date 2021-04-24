4682b1f2b7f4477f04156a042d9d6ddb7d3c03a5
cipitelliarthur <cipitelliarthurgabriel@protonmail.com>
2021-04-24 17:33:32 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,10 +7,12 @@ function semana() {
     let terceiro_misterio = window.document.getElementById('terceiro_misterio')
     let quarto_misterio = window.document.getElementById('quarto_misterio')
     let quinto_misterio = window.document.getElementById('quinto_misterio')
+    let image = window.document.getElementById('image')
 
     if (dia_atual == 1 || dia_atual == 4) {
         document.body.style.background = '#EAC102'
         misterios.innerHTML = `Mistérios Gozosos`
+        img.setAttribute('src', 'imagens/anunciação.png')
         primeiro_misterio.innerHTML = `Anunciação da Bem-Aventurada Virgem Maria`
         segundo_misterio.innerHTML = `Visitação da Bem-Aventurada Virgem Maria`
         terceiro_misterio.innerHTML = `O nascimento de Jesus Cristo Nosso Senhor`
@@ -20,6 +22,7 @@ function semana() {
     } else if (dia_atual == 2 || dia_atual == 5) {
         document.body.style.background = '#ED1C24'
         misterios.innerHTML = `Mistérios Dolorosos`
+        img.setAttribute('src', 'imagens/agonia.png')
         primeiro_misterio.innerHTML = `A Oração e Agonia de Jesus Cristo Nosso Senhor`
         segundo_misterio.innerHTML = `A Flagelação de Jesus Cristo Nosso Senhor`
         terceiro_misterio.innerHTML = `A Coroação de Espinhos de Jesus Cristo Nosso Senhor`
@@ -29,6 +32,7 @@ function semana() {
     } else if (dia_atual == 3 || dia_atual == 6 || dia_atual == 0) {
         document.body.style.background = '#E6E6E6'
         misterios.innerHTML = `Mistérios Gloriosos`
+        img.setAttribute('src', '../imagens/ressureição.png')
         primeiro_misterio.innerHTML = `A Ressurreição de Jesus Cristo Nosso Senhor`
         segundo_misterio.innerHTML = `A Ascensão de Jesus Cristo Nosso Senhor aos Céus`
         terceiro_misterio.innerHTML = `A Vindo do Espírito Santo`
@@ -45,4 +49,7 @@ function semana() {
         quinto_misterio.innerHTML = `ERRO!`
 
     }
+
+    image.style.textAlign = 'center'
+    image.appendChild(img)
 }
