8db23f15df8512d084ee523f482225bdf5a5dafb
cipitelliarthur <cipitelliarthurgabriel@protonmail.com>
2021-04-24 11:20:13 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
 function semana() {
-    let dia_atual = 
+    let data = new Date()
+    let dia_atual = data.getDay()
+    let res = window.document.getElementsByTagName('body')
+
+    res.innerText = `${dia_atual}`
 }
